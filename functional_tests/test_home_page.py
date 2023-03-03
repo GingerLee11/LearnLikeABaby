f3a7f17774a316f50837de89719c1c301c70dae3
Scott Clements <clemencyoflee@gmail.com>
2023-03-03 12:29:42 -0800

    Extract commonly used functionality into base.py file for functional tests.

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 import unittest
 import time
 
+from .base import FunctionalTest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(FunctionalTest):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -16,11 +20,22 @@ class NewVisitorTest(unittest.TestCase):
         # New user Akane visits learnlikeababy.com
         self.browser.get('http://localhost:8000/')
 
-        # Sees Learn Like A Baby in the title of the page
-        self.assertIn('Learn Like A Baby', self.browser.title)
-        time.sleep(4)
-        
+        # Sees Learn Like A Baby in the browser title of the page
+        self.wait_for(lambda:
+            self.assertIn('Learn Like A Baby', self.browser.title)
+        )
 
+        # Sees the title of the home page
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('Learn Like A Baby', header_text)
 
-if __name__ == '__main__':  
-    unittest.main()
+        # Sees the text explaining the name of the blog
+        body = self.browser.find_element(By.TAG_NAME, 'body').text
+
+        ## This might change if the blog is updated, 
+        # but at least I can test to make sure the content doesn't disappear
+        self.assertIn('A blog about learning. Why like a baby?', body)
+        self.assertIn("You can't fill a cup that is already full.", body)
+
+        self.fail('Finish the Test!')
+

5d975f27c720cc2ba85411648d4a25c560f005d4
Scott Clements <clemencyoflee@gmail.com>
2023-03-03 10:47:55 -0800

    First failing test for finding the website name in the title.

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'The install' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_view_home_page_information(self):
+        # New user Akane visits learnlikeababy.com
+        self.browser.get('http://localhost:8000')
+
+        # Sees Learn Like A Baby in the title of the page
+        self.assertIn(self.browser.title, 'Learn Like A Baby')
+        
+
+
+if __name__ == '__main__':  
+    unittest.main()

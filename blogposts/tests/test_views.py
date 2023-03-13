844c224ce5da629836e7f388a12c72f2c3893f46
Scott Clements <clemencyoflee@gmail.com>
2023-03-13 15:24:01 -0700

    Initial commit for blogpost app

---
+++ blogposts/tests/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class TestBlogPostListPage(TestCase):
+
+    def test_uses_blog_post_list_template(self):
+        response = self.client.get('/blogposts')
+        self.assertTemplateUsed(response, 'blogpost_list.html')
+

39c690bc64d2837d59dd2106703d1bc6bdf29d37
Scott Clements <clemencyoflee@gmail.com>
2023-03-13 17:44:11 -0700

    Write tests for BlogPost model and list view

--- blogposts/tests/test_models.py
+++ blogposts/tests/test_models.py
@@ -1,3 +1,188 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from blogposts.models import BlogPost, Category, Language
+
+User = get_user_model()
+
+
+class CategoryModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Category.objects.create(name='Test Category')
+
+    def test_name_label(self):
+        category = Category.objects.get(id=1)
+        field_label = category._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'Category Name')
+
+    def test_name_max_length(self):
+        category = Category.objects.get(id=1)
+        max_length = category._meta.get_field('name').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_object_name_is_name(self):
+        category = Category.objects.get(id=1)
+        expected_object_name = f'{category.name}'
+        self.assertEqual(expected_object_name, str(category))
+
+
+class LanguageModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Language.objects.create(name='Test Language')
+
+    def test_name_label(self):
+        language = Language.objects.get(id=1)
+        field_label = language._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'Language Name')
+
+    def test_name_max_length(self):
+        language = Language.objects.get(id=1)
+        max_length = language._meta.get_field('name').max_length
+        self.assertEqual(max_length, 150)
+
+    def test_object_name_is_name(self):
+        language = Language.objects.get(id=1)
+        expected_object_name = f'{language.name}'
+        self.assertEqual(expected_object_name, str(language))
+
+
+
+class BlogPostTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username='testuser', password='testpass'
+        )
+        cls.category1 = Category.objects.create(name='Category 1')
+        cls.category2 = Category.objects.create(name='Category 2')
+        cls.language1 = Language.objects.create(name='English')
+        cls.language2 = Language.objects.create(name='French')
+
+        cls.blogpost1 = BlogPost.objects.create(
+            title='Test Blog Post 1',
+            sub_title='Test Sub-title 1',
+            author=cls.user,
+            content='Test Content 1'
+        )
+        cls.blogpost1.category.add(cls.category1)
+        cls.blogpost1.category.add(cls.category2)
+        cls.blogpost1.language.add(cls.language1)
+        cls.blogpost1.language.add(cls.language2)
+
+        cls.blogpost2 = BlogPost.objects.create(
+            title='Test Blog Post 2',
+            sub_title='Test Sub-title 2',
+            author=cls.user,
+            content='Test Content 2'
+        )
+        cls.blogpost2.category.add(cls.category2)
+        cls.blogpost2.language.add(cls.language2)
+
+        cls.blogpost3 = BlogPost.objects.create(
+            title='Test Blog Post 3',
+            sub_title='Test Sub-title 3',
+            author=cls.user,
+            content='Test Content 3'
+        )
+        cls.blogpost3.category.add(cls.category1, cls.category2)
+        cls.blogpost3.language.add(cls.language1, cls.language2)
+
+    
+    def test_title_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'Title')
+
+    def test_title_max_length(self):
+        blogpost = BlogPost.objects.get(id=1)
+        max_length = blogpost._meta.get_field('title').max_length
+        self.assertEqual(max_length, 150)
+
+    def test_sub_title_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('sub_title').verbose_name
+        self.assertEqual(field_label, 'Sub-title')
+
+    def test_sub_title_max_length(self):
+        blogpost = BlogPost.objects.get(id=1)
+        max_length = blogpost._meta.get_field('sub_title').max_length
+        self.assertEqual(max_length, 300)
+
+    def test_category_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('category').verbose_name
+        self.assertEqual(field_label, 'category')
+
+    def test_language_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('language').verbose_name
+        self.assertEqual(field_label, 'language')
+
+    def test_author_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('author').verbose_name
+        self.assertEqual(field_label, 'author')
+
+    def test_date_published_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('date_published').verbose_name
+        self.assertEqual(field_label, 'Published')
+
+    def test_date_modified_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('date_modified').verbose_name
+        self.assertEqual(field_label, 'Last Modified')
+
+    def test_content_label(self):
+        blogpost = BlogPost.objects.get(id=1)
+        field_label = blogpost._meta.get_field('content').verbose_name
+        self.assertEqual(field_label, 'Content')
+
+    def test_object_name_is_title_and_sub_title(self):
+        blogpost = BlogPost.objects.get(id=1)
+        expected_object_name = f'Title: {blogpost.title}, Sub-title: {blogpost.sub_title}'
+        self.assertEqual(expected_object_name, str(blogpost))
+
+    def test_ordering(self):
+        expected_ordering = [
+            self.blogpost3,
+            self.blogpost2,
+            self.blogpost1,
+        ]
+        qs = BlogPost.objects.all()
+        self.assertQuerysetEqual(qs, expected_ordering, ordered=True)
+
+    def test_category_name(self):
+        category1 = Category.objects.get(id=self.category1.id)
+        category2 = Category.objects.get(id=self.category2.id)
+        self.assertEqual(str(category1), "Category 1")
+        self.assertEqual(str(category2), "Category 2")
+
+    def test_language_name(self):
+        language1 = Language.objects.get(id=self.language1.id)
+        language2 = Language.objects.get(id=self.language2.id)
+        self.assertEqual(str(language1), "English")
+        self.assertEqual(str(language2), "French")
+
+    def test_blog_post_fields(self):
+        blog_post = BlogPost.objects.get(id=self.blogpost1.id)
+        self.assertEqual(blog_post.title, "Test Blog Post 1")
+        self.assertEqual(blog_post.sub_title, "Test Sub-title 1")
+        self.assertEqual(blog_post.author, self.user)
+        self.assertEqual(blog_post.content, "Test Content 1")
+        self.assertEqual(blog_post.category.count(), 2)
+        self.assertIn(self.category1, blog_post.category.all())
+        self.assertIn(self.category2, blog_post.category.all())
+        self.assertEqual(blog_post.language.count(), 2)
+        self.assertIn(self.language1, blog_post.language.all())
+        self.assertIn(self.language2, blog_post.language.all())
+
+    def test_blog_post_dates(self):
+        blog_post = BlogPost.objects.get(id=self.blogpost1.id)
+        self.assertLess(blog_post.date_published, timezone.now())
+        self.assertLess(blog_post.date_modified, timezone.now())
 
 
--- blogposts/tests/test_views.py
+++ blogposts/tests/test_views.py
@@ -1,10 +1,74 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from datetime import timedelta
+
+from blogposts.models import BlogPost
+from blogposts.views import BlogPostListView
 
 
-class TestBlogPostListPage(TestCase):
+class BlogPostListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 2 blog posts for testing
+        cls.blogpost1 = BlogPost.objects.create(
+            title='Test Blog Post 1',
+            content='Test Content 1'
+        )
+        cls.blogpost2 = BlogPost.objects.create(
+            title='Test Blog Post 2',
+            content='Test Content 2'
+        )
 
-    def test_uses_blog_post_list_template(self):
-        response = self.client.get('/blogposts')
+    def test_blogpost_list_view_status_code(self):
+        response = self.client.get(reverse('blogpost-list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_blogpost_list_view_template_name(self):
+        response = self.client.get(reverse('blogpost-list'))
         self.assertTemplateUsed(response, 'blogposts/blogpost_list.html')
 
-    
+    def test_blogpost_list_view_queryset(self):
+        response = self.client.get(reverse('blogpost-list'))
+        self.assertQuerysetEqual(
+            response.context['object_list'],
+            [self.blogpost2, self.blogpost1]
+        )
+
+    def test_blogpost_list_view_pagination(self):
+        # Create 8 additional blog posts
+        for i in range(8):
+            BlogPost.objects.create(
+                title=f'Test Blog Post {i+3}',
+                content=f'Test Content {i+3}'
+            )
+        response = self.client.get(reverse('blogpost-list'))
+        self.assertEqual(len(response.context['object_list']), 10)
+
+    def test_blogpost_list_view_ordering(self):
+        BlogPost.objects.all().delete()
+        # Create 3 blog posts with different dates
+        blogpost1 = BlogPost.objects.create(
+            title='Test Blog Post 1',
+            content='Test Content 1'
+        )
+        blogpost2 = BlogPost.objects.create(
+            title='Test Blog Post 2',
+            content='Test Content 2'
+        )
+        blogpost3 = BlogPost.objects.create(
+            title='Test Blog Post 3',
+            content='Test Content 3'
+        )
+        blogpost3.date_published = blogpost1.date_published + timedelta(days=1)
+        blogpost3.save()
+        blogpost2.date_published = blogpost1.date_published - timedelta(days=1)
+        blogpost2.save()
+
+        response = self.client.get(reverse('blogpost-list'))
+
+        self.assertQuerysetEqual(
+            response.context['object_list'],
+            [blogpost3, blogpost1, blogpost2]
+        )
+
